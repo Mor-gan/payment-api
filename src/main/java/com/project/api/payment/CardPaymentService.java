37f7de8a2331fe92305aeed282a48b2b1949a41c
Mor-gan <ogundipeabimbola9@gmail.com>
2023-12-30 11:39:42 +0000

    get all  method

--- src/main/java/com/project/api/payment/CardPaymentService.java
+++ src/main/java/com/project/api/payment/CardPaymentService.java
@@ -1,12 +1,27 @@
 package com.project.api.payment;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CardPaymentService {
 
-     List<CardPayment> getAllPayments() {
-      return null;
-   }
+    public static List<CardPayment> getAllPayments(String filePathString) {
 
-   
+        List<CardPayment> cardPayments = new ArrayList<>();
+        try {
+            File sourceFile = Paths.get(filePathString).toFile();
+            ObjectMapper objectMapper = new ObjectMapper();
+            cardPayments = Arrays.asList(objectMapper.readValue(sourceFile, CardPayment[].class));
+            cardPayments.forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return cardPayments;
+    }
 }
